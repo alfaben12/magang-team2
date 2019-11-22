cff6e4e04c92c6dd715fa04a1ac3cff08c27ac21
Satya <ucihanet13@gmail.com>
2019-11-22 14:18:37 +0700

    Koneksi.php

---
+++ Tugas membuat register/Koneksi.php
@@ -0,0 +1,7 @@
+<?php
+   $hostname  = "localhost";
+   $username  = "root";
+   $password  = "";
+   $dbname  = "training";
+   $db = new mysqli($hostname, $username, $password, $dbname) or die("koneksi gagal");
+?>
